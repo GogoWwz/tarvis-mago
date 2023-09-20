e9ad970f3b1043cc703f1c80371db03b4acfa52b
GogoWwz <wwz_zjut_web@163.com>
2023-09-20 21:07:03 +0800

    feat: mago v0.0.1

--- background/index.js
+++ background/index.js
@@ -1,31 +1,32 @@
-// chrome.runtime.onMessage.addListener((message) => {
-//   switch (message.type) {
-//     case 'capture-start': captureStart(); break;
-//   }
-// })
-
-chrome.action.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener(() => {
   captureStart();
 })
 
 const captureStart = () => {
-  chrome.tabs.captureVisibleTab(chrome.windows.WINDOW_ID_CURRENT, {
-    format: 'png'
-  }, (dataUrl) => {
-    console.log('captureStart')
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, (tabs) => {
-      const message = {
-        type: 'capture-complete',
-        data: {
-          dataUrl
+  chrome.windows.getCurrent(function (window) {
+    var currentWindowId = window.id;
+    console.log("Current window ID: " + currentWindowId);
+
+    chrome.tabs.captureVisibleTab(currentWindowId, {
+      format: 'png'
+    }, (dataUrl) => {
+      console.log('captureStart')
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, (tabs) => {
+        const message = {
+          type: 'capture-complete',
+          data: {
+            dataUrl
+          }
         }
-      }
-      chrome.tabs.sendMessage(tabs[0].id, message, res => {
-        // response
+        console.log(tabs)
+        console.log(dataUrl)
+        chrome.tabs.sendMessage(tabs[0].id, message, {}, res => {
+          // response
+        })
       })
     })
-  })
+  });
 }
--- content-scripts/index.js
+++ content-scripts/index.js
@@ -5,10 +5,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   sendResponse('done');
 })
 
-const { createWorker } = Tesseract;
-
 const captureComplete = async ({ dataUrl }) => {
   console.log('captureComplete')
+  // 在背景页中打印设备像素比
+  console.log("window.devicePixelRatio: ", window.devicePixelRatio);
+
+  console.log(dataUrl)
 
   let flag = false
   let startX = 0;
@@ -22,7 +24,7 @@ const captureComplete = async ({ dataUrl }) => {
   seleciton.setAttribute('id', 'selection')
 
   function selectStart(e) {
-    // console.log("mousedown", e)
+    console.log("mousedown", e)
     flag = true;
     seleciton.style['left'] = e.pageX + 'px';
     seleciton.style['top'] = e.pageY + 'px';
@@ -40,7 +42,7 @@ const captureComplete = async ({ dataUrl }) => {
   }
 
   async function selectEnd(e) {
-    // console.log("mouseup", e)
+    console.log("mouseup", e)
     flag = false
     endX = e.pageX
     endY = e.pageY
@@ -52,16 +54,41 @@ const captureComplete = async ({ dataUrl }) => {
     removeSelectElement();
 
     const imageSrc = await cropImage(dataUrl, startX, startY, Math.abs(e.pageX - startX), Math.abs(e.pageY - startY));
-    const worker = await createWorker({
-      logger: m => console.log(m)
-    });
+    console.log(imageSrc)
+    const imgageBlob = base64ToBlob(imageSrc)
+    try {
+      const formData = new FormData();
+      formData.append('file', imgageBlob, 'capture.jpg');
 
-    await worker.loadLanguage("chi_sim");
-    await worker.initialize("chi_sim");
+      console.log(formData.get('file'))
 
-    console.log(worker)
-    const { data: { text } } = await worker.recognize(imageSrc);
-    console.log(text);
+      const res = await fetch('https://tarvisai.cn/api/skill/ocr', {
+        method: 'POST',
+        body: formData,
+        // 不能加这个，不然会出现没有boundary的错误
+        // headers: {
+        //   'Content-Type': 'multipart/form-data',
+        // },
+      });
+      if (res.ok) {
+        const { data } = await res.json()
+        const div = document.createElement('div')
+        const h2 = document.createElement('h2')
+        const p = document.createElement('p')
+        div.setAttribute('id', 'result')
+        h2.innerHTML = 'Tarvis识别完成，已为您复制到剪贴板！'
+        p.innerHTML = data
+        div.append(h2)
+        div.append(p)
+        document.body.append(div)
+        copyToClipboard(data);
+        setTimeout(() => {
+          document.body.removeChild(div)
+        }, 3 * 1000)
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   function removeSelectElement() {
@@ -76,19 +103,34 @@ const captureComplete = async ({ dataUrl }) => {
   }
 
   function cropImage(dataUrl, startX, startY, width, height) {
+    const ratio = window.devicePixelRatio;
     return new Promise((resolve) => {
       let img = new Image();
       img.src = dataUrl;
       img.onload = function () {
         let canvas = document.createElement('canvas')
         let ctx = canvas.getContext('2d')
-        canvas.width = width;
-        canvas.height = height;
-        ctx.drawImage(img, -startX, -startY)
-        resolve(canvas.toDataURL('png'))
+        canvas.width = width * ratio;
+        canvas.height = height * ratio;
+        ctx.drawImage(img, -startX * ratio, -startY * ratio)
+        resolve(canvas.toDataURL('image/jpeg', 1))
       };
     })
   };
+
+  function base64ToBlob(base64Data) {
+    const parts = base64Data.split(';base64,');
+    const contentType = parts[0].split(':')[1];
+    const raw = window.atob(parts[1]);
+    const rawLength = raw.length;
+    const uInt8Array = new Uint8Array(rawLength);
+  
+    for (let i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+  
+    return new Blob([uInt8Array], { type: contentType });
+  }
 
   document.body.addEventListener('mousedown', selectStart)
   document.body.addEventListener('mousemove', selectMoving)
@@ -96,4 +138,25 @@ const captureComplete = async ({ dataUrl }) => {
 
   document.body.append(div)
   document.body.append(seleciton)
+}
+
+// 创建一个辅助函数，用于将内容复制到剪贴板
+function copyToClipboard(text) {
+  // 创建一个临时的文本输入框元素
+  const textarea = document.createElement('textarea');
+  
+  // 设置文本输入框的值为要复制的文本
+  textarea.value = text;
+  
+  // 将文本输入框添加到文档中
+  document.body.appendChild(textarea);
+  
+  // 选择文本输入框中的内容
+  textarea.select();
+  
+  // 将选择的内容复制到剪贴板
+  document.execCommand('copy');
+  
+  // 移除临时的文本输入框
+  document.body.removeChild(textarea);
 }
